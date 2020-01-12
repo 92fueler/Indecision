192cf0d16d041d6fc231acab3e72bbcab0acc4fa
92fueler <jianjoycewu@gmail.com>
2020-01-12 15:59:29 -0800

    default props value

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ class IndecisionApp extends React.Component{
         this.handlePick = this.handlePick.bind(this);
         this.handleAddOption = this.handleAddOption.bind(this);
         this.state = {
-            options: []
+            options: props.options
         };
     }
 
@@ -40,12 +40,11 @@ class IndecisionApp extends React.Component{
     }
 
     render(){
-        const title = 'Indecision';
         const subtitle = 'Put your life in the hands of a computer';
 
         return (
             <div>
-                <Header title={title} subtitle={subtitle} />
+                <Header subtitle={subtitle} />
                 <Action 
                     hasOptions={this.state.options.length > 0} 
                     handlePick={this.handlePick}
@@ -62,14 +61,22 @@ class IndecisionApp extends React.Component{
     }
 }
 
+IndecisionApp.defaultProps = {
+    options: []
+}
+
 
 const Header = (props) => {
     return (
         <div>
             <h1>{props.title}</h1>
-            <h2>{props.subtitle}</h2>
+            {props.subtitle && <h2>{props.subtitle}</h2>}
         </div>
     );
+}
+
+Header.defaultProps = {
+    title: 'Indecision'
 }
 
 
@@ -158,4 +165,4 @@ class AddOption extends React.Component {
 }
 
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp options={['Devils den', 'Second district']} />, document.getElementById('app'));
